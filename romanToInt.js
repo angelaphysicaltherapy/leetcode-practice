d8511992118efd3ab3033739e2d0dce0cc64c738
anqizhang <angela.cheung1026@gmail.com>
2022-05-02 15:52:04 -0500

    isNot solution

--- romanToInt.js
+++ romanToInt.js
@@ -14,9 +14,16 @@ var romanToInt = function (s) {
         "M": 1000
     }
 
+
     //start a sum with the last roman numeral
     //sum each character, unless next character is of greater value, then subtract 
     let sum = values[s[s.length - 1]];
+    if(!s.split("").every(el=>Object.keys(values).includes(el))){
+        return "Not valid roman input."
+    }
+    if(s.length===0){
+        return ""
+    }
     for (let i = 0; i < s.length - 1; i++) {
         const current = values[s[i]];
         const next = values[s[i + 1]];
@@ -30,3 +37,5 @@ var romanToInt = function (s) {
 
     return sum;
 };
+
+console.log(romanToInt(""))
